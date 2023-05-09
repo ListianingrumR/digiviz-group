3240c8f062f9df0e21c846f86dcf0f26689ace15
Eriswang <ucfnany@ucl.ac.uk>
2023-05-09 19:39:40 +0100

    haha

--- gobal-vis/pre_clean_data.R
+++ gobal-vis/pre_clean_data.R
@@ -7,3 +7,9 @@ library(tidyverse)
 library(dplyr)
 library(sf)
 library(skimr)
+
+word_sh <- st_read("D:/003_digital_visualisation/group_cw2/A_group-work/digiviz-group/gobal-data/world-administrative-boundaries.geojson")
+
+world-gdp <- left_join(word_sh, each_country_gdp,by = "name")
+
+write.csv(each_country_gdp,file='D:/003_digital_visualisation/group_cw2/A_group-work/digiviz-group/gobal-data/world_gdp_2000.csv',fileEncoding = "UTF-8")
