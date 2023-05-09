0dd492c65fc57b8f7d586edc72bd8e6361427af5
derp015 <LYR15968@gmail.com>
2023-05-09 14:54:10 +0100

    framework for Australia chapters and charts

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ let bylineDiv = "<p class='byline'>by Asian Geeks<br>(Juerong Wang, Rahmadita Li
 
 // put the description here (can embed video and pictures)
 let descriptionDiv =
-  "<p><img src='img/crisis.jpg'></p>"+
+  "<p><img src='img/crisis.jpg'></p>" +
   "<p style=margin-top:50px>Introduction</p>" +
   '<p style="text-align:center;margin-top:100px">Lets scroll!<br>▼</p>';
 
@@ -23,28 +23,39 @@ let footerDiv =
 
 // put the 1st box navigation here
 let divChapter1 =
-"<p><b>Financial Crisis - The History</b></p>" + "<hr>" +
-"<p><img src='img/the-great-recession.png'></p>"+ "<hr>" +
-// https://www.kent.ac.uk/business-and-partnerships/news-and-events/922/the-impact-of-cost-cutting-actions-on-employee-relations-and-workplace-grievances-what-weve-learned-from-the-great-recession
-"<p><b>The Great Recession: </b></p>"
+  "<p><b>Financial Crisis - The History</b></p>" + "<hr>" +
+  "<p><img src='img/the-great-recession.png'></p>" + "<hr>" +
+  // https://www.kent.ac.uk/business-and-partnerships/news-and-events/922/the-impact-of-cost-cutting-actions-on-employee-relations-and-workplace-grievances-what-weve-learned-from-the-great-recession
+  "<p><b>The Great Recession: </b></p>"
 
 // continue to next box/chapter
 let divChapter2 =
   "<p><b>Where it starts</b></p>" + "<hr>" +
-  "<p><img src='img/lehmanbrothers.jpg'></p>"+ "<hr>" +
+  "<p><img src='img/lehmanbrothers.jpg'></p>" + "<hr>" +
   // https://www.thebalancemoney.com/lehman-brothers-collapse-causes-impact-4842338
   "<p><b>Lehman Brothers Bankcruptcy</b></p>"
-  //"<p><img src='images/jakarta-waste-volume-per-day-by-type-ton.png'></p>"+
-  //"<p class='data-source'>Data source: <a href='https://datawrapper.dwcdn.net/m2o0s/3/'>Environment Office DKI Jakarta</a></p>"+
-  //"<p>Jakarta is divided into 6 districts, which are Thousand Islands, Central Jakarta, North Jakarta, West Jakarta, East Jakarta and South Jakarta.</p>"+
-  //"<p><img src='images/jakarta-waste-generation-by-district-per-day-ton.png'></p>"+
-  //"<p class='data-source'>Data source: <a href='https://datawrapper.dwcdn.net/o50nX/1/'>Indonesian Ministry of Environment and Forestry</a></p>";
-  //https://datawrapper.dwcdn.net/o50nX/1/
+//"<p><img src='images/jakarta-waste-volume-per-day-by-type-ton.png'></p>"+
+//"<p class='data-source'>Data source: <a href='https://datawrapper.dwcdn.net/m2o0s/3/'>Environment Office DKI Jakarta</a></p>"+
+//"<p>Jakarta is divided into 6 districts, which are Thousand Islands, Central Jakarta, North Jakarta, West Jakarta, East Jakarta and South Jakarta.</p>"+
+//"<p><img src='images/jakarta-waste-generation-by-district-per-day-ton.png'></p>"+
+//"<p class='data-source'>Data source: <a href='https://datawrapper.dwcdn.net/o50nX/1/'>Indonesian Ministry of Environment and Forestry</a></p>";
+//https://datawrapper.dwcdn.net/o50nX/1/
 
+
+// YR codes
 let divChapter3 =
   "<p><b>Australia</b></p>" + "<hr>" +
-  "<p>Area:  km²</p>"+
-  "<p>Total Population: </p>";
+  "<p>Area 2023: 7,692,024 km²</p>" +
+  "<p>Total Population 2023 : 26,339,393 </p>" +
+  '<div id="chart-container"></div>' +
+  '<iframe src="chart.html" width="100%" height="300px"></iframe>';
+
+
+let divChapter4 =
+  "<p><b>Sydney</b></p>" + "<hr>" +
+  "<a href='https://www.macrotrends.net/cities/206167/sydney/population'>Sydney, Australia Metro Area Population 1950-2023</a>";
+
+//YR
 
 // the var section is to navigate the map in each chapter/box
 var config = {
@@ -68,7 +79,7 @@ var config = {
       hidden: false,
       chapterDiv: divChapter1,
       location: { // identify the location, the zoom scale, the pitch and bearing
-        center: [186.328125,39.095963], // use bbox finder
+        center: [186.328125, 39.095963], // use bbox finder
         zoom: 1.5,
         zoomSmall: 1,
         pitch: 0,
@@ -92,7 +103,7 @@ var config = {
       hidden: false,
       chapterDiv: divChapter2,
       location: {
-        center: [260.683594,39.571822],
+        center: [260.683594, 39.571822],
         zoom: 2.5,
         zoomSmall: 2,
         pitch: 0,
@@ -116,6 +127,7 @@ var config = {
         },
       ],
     },
+    //YR
     {
       id: "Australia",
       alignment: "right",
@@ -125,8 +137,8 @@ var config = {
       description: "",
       chapterDiv: divChapter3,
       location: {
-        center: [106.58067, -5.79391],
-        zoom: 10,
+        center: [151.2093, -33.8688],
+        zoom: 3.5,
         zoomSmall: 3,
         pitch: 0,
         bearing: 0,
@@ -140,54 +152,103 @@ var config = {
           opacity: 0,
           duration: 300,
         },
+      ],
+      onChapterExit: [
         {
-          layer: "saringan-sampah-sungai",
+          layer: "tps",
           opacity: 0,
           duration: 300,
         },
+      ],
+    },
+    {
+      id: "Sydney",
+      alignment: "right",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter4,
+      location: {
+        center: [151.2093, -33.8688],
+        zoom: 9,
+        zoomSmall: 3,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
         {
-          layer: "timbulandki22",
-          opacity: 0,
-          duration: 300,
-        },
-        {
-          layer: "seribu",
-          opacity: 1,
-          duration: 300,
-        },
-        {
-          layer: "jakpus",
-          opacity: 0,
-          duration: 300,
-        },
-        {
-          layer: "jakut",
-          opacity: 0,
-          duration: 300,
-        },
-        {
-          layer: "jakbar",
-          opacity: 0,
-          duration: 300,
-        },
-        {
-          layer: "jaktim",
-          opacity: 0,
-          duration: 300,
-        },
-        {
-          layer: "jaksel",
+          layer: "tps",
           opacity: 0,
           duration: 300,
         },
       ],
       onChapterExit: [
         {
-          layer: "seribu",
+          layer: "tps",
           opacity: 0,
           duration: 300,
         },
       ],
     },
+    //YR
+    /*
+         {
+           layer: "tps",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "saringan-sampah-sungai",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "timbulandki22",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "seribu",
+           opacity: 1,
+           duration: 300,
+         },
+         {
+           layer: "jakpus",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "jakut",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "jakbar",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "jaktim",
+           opacity: 0,
+           duration: 300,
+         },
+         {
+           layer: "jaksel",
+           opacity: 0,
+           duration: 300,
+         },
+       ],
+       onChapterExit: [
+         {
+           layer: "seribu",
+           opacity: 0,
+           duration: 300,
+         },
+       ],
+     },*/
   ],
 };
